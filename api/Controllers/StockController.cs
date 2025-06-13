a7dbf94fd56dc820fba363c92dbdc49f764c1019
mfelipegs <mfelipegs.dev@gmail.com>
2025-06-13 03:26:41 -0300

    feat: create stock item dto, mapper and controller

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Dtos.Stock;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
@@ -32,12 +33,24 @@ namespace api.Controllers
         {
             var stock = _context.Stock.Find(id);
 
-            if(stock == null)
+            if (stock == null)
             {
-                return NotFound();  
+                return NotFound();
             }
 
             return Ok(stock.ToStockDto());
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+        {
+            var stockModel = stockDto.ToStockFromCreateDTO();
+
+            _context.Stock.Add(stockModel);
+
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
         }
     }
 }
