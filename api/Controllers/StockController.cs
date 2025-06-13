8ac00eb5d8a886acfee3ada37e27a95221b701b7
mfelipegs <mfelipegs.dev@gmail.com>
2025-06-13 03:08:06 -0300

    feat: adding dto to remove comments on get return

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -21,7 +22,7 @@ namespace api.Controllers
 
         public IActionResult GetAll()
         {
-            var stocks = _context.Stock.ToList();
+            var stocks = _context.Stock.ToList().Select(s => s.ToStockDto());
 
             return Ok(stocks);
         }
@@ -36,7 +37,7 @@ namespace api.Controllers
                 return NotFound();  
             }
 
-            return Ok(stock);
+            return Ok(stock.ToStockDto());
         }
     }
 }
