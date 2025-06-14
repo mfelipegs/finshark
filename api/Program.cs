8ed270ebfc1ff041f3718b8471bf9cf5e768b895
mfelipegs <mfelipegs.dev@gmail.com>
2025-06-14 13:57:41 -0300

    feat: including comments on stock getbyid and get endpoints

--- api/Program.cs
+++ api/Program.cs
@@ -11,6 +11,11 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddControllers();
 
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+});
+
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
