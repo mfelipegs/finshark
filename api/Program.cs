17f404de32f65770bbbf8e7d5ebbf02bedfa510b
mfelipegs <matheusg.dasilva7@gmail.com>
2025-06-17 23:42:17 -0300

    feat: comments query params and enabling cors

--- api/Program.cs
+++ api/Program.cs
@@ -74,6 +74,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials()
+    //.WithOrigins("https://localhost:5109"))
+    .SetIsOriginAllowed(origin => true)
+);
+
 app.UseAuthentication();
 app.UseAuthorization();
 
